709210c1bd625da14a92fa8cf79ab752b65ab566
Azadshekhar <shekharmaurya8978@gmail.com>
2024-07-24 23:25:11 +0530

    first commit

---
+++ app/src/main/java/com/example/openinappassignment/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.openinappassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        val nevController=findNavController(R.id.fragment)
+        bottomNavigationView.setupWithNavController(nevController)
+    }
+}
